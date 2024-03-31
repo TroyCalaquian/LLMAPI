c74e64c6196ddc142cfff6949d22647d964c5ba9
LaggySoul <trcalaquian@gmail.com>
2024-03-30 23:25:18 -0700

    Fixed imports

--- model.py
+++ model.py
@@ -1,8 +1,8 @@
-from transformers import AutoTokenizer, AutoModelForSequenceClassification, pipeline
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, pipeline
 
 model_id = "facebook/blenderbot-400M-distill"
 tokenizer = AutoTokenizer.from_pretrained(model_id, legacy = False)
-model = AutoModelForSequenceClassification.from_pretrained(model_id)
+model = AutoModelForSeq2SeqLM.from_pretrained(model_id)
 chatbot_pipeline = pipeline("text2text-generation", model=model, tokenizer=tokenizer)
 
 def use_pipeline(text):
